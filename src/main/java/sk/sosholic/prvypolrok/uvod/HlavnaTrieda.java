700187d9f55664fce852b7fa959e16c2681ab018
Dado <petkodado2005@gmail.com>
2022-09-22 08:13:59 +0200

    commit

--- src/main/java/sk/sosholic/prvypolrok/uvod/HlavnaTrieda.java
+++ src/main/java/sk/sosholic/prvypolrok/uvod/HlavnaTrieda.java
@@ -3,6 +3,6 @@ package sk.sosholic.prvypolrok.uvod;
 public class HlavnaTrieda {
     public static void main(String[] args) {
         System.out.println("test");
-
+        // test dva tri
     }
 }
